84fe39b65323fd5a141ca73d90e64c27203f77f2
santiagorodriguez98 <68866132+santiagorodriguez98@users.noreply.github.com>
2020-08-11 16:46:54 -0300

    edito el js de los productos
    Creé la función showProductsList

--- js/products.js
+++ js/products.js
@@ -1,6 +1,28 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+var productsArray = [];
 
+function showProductsList(array){
+
+    let contenido = "";
+    for(let i = 0; i < array.length; i++){
+        let category = array[i];
+        contenido += category.name + '<br>';
+        contenido += category.description + '<br>';
+        contenido += category.cost;
+        contenido += '<br><hr><br>';
+
+        document.getElementById('container p-5').innerHTML = contenido;
+        }
+    } 
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productsArray = resultObj.data; 
+            showProductsList(productsArray);
+        }
+
+});
 });
