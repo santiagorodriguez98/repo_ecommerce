e159bbadc100ee629ef8217c2525460c3b3f481e
santiagorodriguez98 <68866132+santiagorodriguez98@users.noreply.github.com>
2020-08-26 17:56:41 -0300

    modifico products.html y js
    le agrego el filtrado por precio de productos

--- js/products.js
+++ js/products.js
@@ -2,29 +2,62 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 var productsArray = [];
+var maxPrice = undefined;
+var minPrice = undefined;
 
-function showProductsList(array){
+function showProductsList(array) {
 
     let contenido = "";
-    for(let i = 0; i < array.length; i++){
+    for (let i = 0; i < array.length; i++) {
         let category = array[i];
-         
-        contenido += category.name + '<br>' ;
-        contenido +=  "<img src=" + category.imgSrc +"><br>";
-        contenido += 'Descripción: ' + category.description + '<br>';
-        contenido += 'Precio: ' + category.cost;
-        contenido += '<br><hr><br>';
-        
+
+
+        if (((minPrice == undefined) || (parseInt(category.cost) >= minPrice)) &&
+            ((maxPrice == undefined) || (parseInt(category.cost) <= maxPrice))) {
+
+            contenido += category.name + '<br>';
+            contenido += "<img src=" + category.imgSrc + "><br>";
+            contenido += 'Descripción: ' + category.description + '<br>';
+            contenido += 'Precio: ' + category.cost + "<br>";
+            contenido += 'Cantidad de vendidos: ' + category.soldCount;
+            contenido += '<br><hr><br>';
+        }
         document.getElementById("divId").innerHTML = contenido;
     }
-} 
+}
 
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCTS_URL).then(function (resultObj) {
         if (resultObj.status === "ok") {
-            productsArray = resultObj.data; 
+            productsArray = resultObj.data;
             showProductsList(productsArray);
         }
 
-    }); 
+    });
+    document.getElementById("Filter").addEventListener("click", function () {
+        minPrice = document.getElementById("minPrice").value;
+        maxPrice = document.getElementById("maxPrice").value;
+        if ((minPrice != undefined) && (minPrice != "") && (parseInt(minPrice) >= 0)) {
+            minPrice = parseInt(minPrice);
+        }
+        else {
+            minPrice = undefined;
+        }
+        if ((maxPrice != undefined) && (maxPrice != "") && (parseInt(maxPrice) >= 0)) {
+            maxPrice = parseInt(maxPrice);
+        }
+        else {
+            maxPrice = undefined;
+        }
+        showProductsList(productsArray);
+    });
+
+    document.getElementById("clean").addEventListener("click", function () {
+        document.getElementById("maxPrice").value = "";
+        document.getElementById("minPrice").value = "";
+        maxPrice = undefined;
+        minPrice = undefined;
+        
+        showProductsList(productsArray);
+    });
 });
