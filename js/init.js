d51b1dc0bd0a3b3630518fe8b43726fb27fd6d91
santiagorodriguez98 <68866132+santiagorodriguez98@users.noreply.github.com>
2020-09-09 02:24:44 -0300

    termino con todas las pautas
    me falta la puntuacion con las estrellas

--- js/init.js
+++ js/init.js
@@ -7,59 +7,59 @@ const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/pro
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
-var showSpinner = function(){
+var showSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "block";
 }
 
-var hideSpinner = function(){
+var hideSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "none";
 }
 
-var getJSONData = function(url){
-    var result = {};
-    showSpinner();
-    return fetch(url)
+var getJSONData = function (url) {
+  var result = {};
+  showSpinner();
+  return fetch(url)
     .then(response => {
       if (response.ok) {
         return response.json();
-      }else{
+      } else {
         throw Error(response.statusText);
       }
     })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          hideSpinner();
-          return result;
+    .then(function (response) {
+      result.status = 'ok';
+      result.data = response;
+      hideSpinner();
+      return result;
     })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        hideSpinner();
-        return result;
+    .catch(function (error) {
+      result.status = 'error';
+      result.data = error;
+      hideSpinner();
+      return result;
     });
 }
 
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
 
-    let userLogged = localStorage.getItem('User-Logged');
-    let infoUser = document.getElementById("info-user");
-    let user = document.getElementById("user");
+  let userLogged = localStorage.getItem('User-Logged');
+  let infoUser = document.getElementById("info-user");
+  let user = document.getElementById("user");
 
-    if (userLogged) {
-      userLogged = JSON.parse(userLogged);
-      user.innerText = user.innerText + "Estas logueado como: " + userLogged.email;
-      infoUser.style = "display: inline-block";  
 
-    }
-    document.getElementById("exit").addEventListener("click",function (){
-      localStorage.removeItem('User-Logged');
-      window.location = "index.html";
-      });
+  if (userLogged) {
+    userLogged = JSON.parse(userLogged);
+    user.innerText = user.innerText + "Estas logueado como: " + userLogged.email;
+    infoUser.style = "display: inline-block";
+    var form = document.getElementById("formDiv");
+    form.style = "display: inline-block";
+  }
+  
+  document.getElementById("exit").addEventListener("click", function (e) {
+    localStorage.removeItem('User-Logged');
+    window.location = "index.html";
+  });
 });
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("click", function(e){
-
-});
--- js/product-info.js
+++ js/product-info.js
@@ -2,22 +2,33 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 var infoProduct = {};
-function showProduct(product) {
+var commentsArray = {};
+function showProduct(infoProduct, commentsArray) {
     let contenido = "";
-    let category = infoProduct;
+    let product = infoProduct;
     let images = "";
+    let comments = "";
 
-    contenido += category.name + '<br><br>';
-    contenido += 'Descripción: <br><br>' + category.description + '<br><br>';
-    contenido += 'Precio: ' + category.cost;
-    contenido += " " + category.currency + "<br>";
-    contenido += 'Cantidad de vendidos: ' + category.soldCount;
+    contenido += product.name + '<br><br>';
+    contenido += 'Descripción: <br><br>' + product.description + '<br><br>';
+    contenido += 'Precio: ' + product.cost;
+    contenido += " " + product.currency + "<br>";
+    contenido += 'Cantidad de vendidos: ' + product.soldCount+'<br>';
+    contenido += 'Categoría: ' + product.category
     contenido += '<br><hr><br>';
 
     images += '<hr><br>' + '<img src = "img/car1.jpg" class = car1>';
     images += '<img src = "img/car2.jpg" class = car2>';
     images += '<img src = "img/car3.jpg" class  = car3>';
 
+    for (let comentarios in commentsArray) {
+        comments += '<br>Usuario: <p style= "font-weight: bold;">'+ commentsArray[comentarios].user + '</p>';
+        comments += 'comentó: ' + commentsArray[comentarios].description + '<br>';
+        comments += 'Su puntuación: '+ commentsArray[comentarios].score + '/5 <br>';
+        comments += 'Publicado: '+ commentsArray[comentarios].dateTime + '<br><br>';
+    }
+    
+    document.getElementById("comments").innerHTML = comments;
     document.getElementById("infoProduct").innerHTML = contenido;
     document.getElementById("productImages").innerHTML = images;
 }
@@ -25,17 +36,21 @@ function showProduct(product) {
 
 
 
+
+
 document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            commentsArray = resultObj.data;
+        }
+    });
+
     getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
         if (resultObj.status === "ok") {
             infoProduct = resultObj.data;
 
-
         }
-        showProduct(infoProduct);
+        showProduct(infoProduct, commentsArray);
     });
-    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-            infoProduct = resultObj.data;
-        }
+    
 });
