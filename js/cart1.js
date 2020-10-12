93a34cd307e08c27aada919b71e36854b21bcb15
santiagorodriguez98 <68866132+santiagorodriguez98@users.noreply.github.com>
2020-10-11 23:42:23 -0300

    entrega 5 completa

--- js/cart1.js
+++ js/cart1.js
@@ -2,31 +2,39 @@ var ProductsArray = [];
 
 function calcTotal() {
     let totalPesos = 0;
+    let totalDolares = 0;
     let subs = document.getElementsByClassName("subtotal");
     for (let i = 0; i < subs.length; i++) {
         totalPesos += parseInt(subs[i].innerHTML);
+        totalDolares += totalPesos/40;
     }
     document.getElementById("totalPesos").innerHTML = totalPesos;
+    document.getElementById("totalDolares").innerHTML = totalDolares;
 }
-function calcSubtotal(precio, i) {
+function calcSubtotal(article, i) {
     let cantidad = parseInt(document.getElementById(`cantidad${i}`).value);
-    let subtotalPesos = cantidad * precio;
-    document.getElementById(`subtotal${i}`).innerHTML = subtotalPesos;
-    
+    let moneda = article.currency;
+    let precio = article.unitCost;
+    if ( moneda = "UYU") {
+      let subtotalPesos = cantidad * precio;
+        document.getElementById(`subtotal${i}`).innerHTML = subtotalPesos;
+    } else {
+    let conversion = precio * 40;
+        let subtotalDolares = cantidad * conversion;
+        document.getElementById(`subtotalDolares${i}`).innerHTML = subtotalDolares;
+    }
     calcTotal();
-
 }
-
-
+    
 
 function showProductsList(array) {
-
+  
     let contenido = '';
     for (let i = 0; i < array.length; i++) {
         let category = array[i];
-
+  
         let sub = category.unitCost * category.count;
-
+      
         contenido += `
         <tr>
             <td>${category.name}</td>
@@ -34,11 +42,12 @@ function showProductsList(array) {
             <td id="precio${i}">${category.currency} ${category.unitCost}</td>
             <td><input style="width:60px;" onchange="calcSubtotal(${category.unitCost},${i})"
                 type="number" id="cantidad${i}" value="${category.count}" min="1"></td>
-            <td><span class="subtotal" id="subtotal${i}" style="font-weight:bold;">${sub}</span></td>
+            <td><span class="subtotal" id="subtotal${i}" style="font-weight:bold;">${sub}</span></td> 
+            <td><span class="subtotal" id="subtotalDolares${i}" style="font-weight:bold;">${sub}</span></td> 
         </tr>`
-
+        
     }
-
+    
     document.getElementById("listado").innerHTML = contenido;
 }
 function calcEnvio() {
@@ -48,7 +57,7 @@ function calcEnvio() {
     let array = document.getElementsByName("envio");
     for (var i = 0; i < array.length; i++) {
         if (array[i].checked) {
-            envio = array[i].value;
+            envio =array[i].value;
         }
     }
 
@@ -64,7 +73,7 @@ function calcEnvio() {
 }
 
 document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(CART_INFO_URL).then(function (resultObj) {
+    getJSONData(CART_INFO_DESAFIO).then(function (resultObj) {
         if (resultObj.status === "ok") {
             ProductsArray = resultObj.data.articles;
 
@@ -74,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
     });
     let array = document.getElementsByName("envio");
     for (var i = 0; i < array.length; i++) {
-        array[i].addEventListener("change", function () {
+        array[i].addEventListener("change", function() {
             calcEnvio();
         });
     }
