1f4f44fb9f7f27a934bcf61842fe67112e079e9f
santiagorodriguez98 <68866132+santiagorodriguez98@users.noreply.github.com>
2020-10-26 00:12:49 -0300

    entrega 6

--- js/cart.js
+++ js/cart.js
@@ -9,14 +9,14 @@ function calcTotal() {
     document.getElementById("totalPesos").innerHTML = totalPesos;
 }
 function calcSubtotal(precio, i) {
+
     let cantidad = parseInt(document.getElementById(`cantidad${i}`).value);
+
     let subtotalPesos = cantidad * precio;
     document.getElementById(`subtotal${i}`).innerHTML = subtotalPesos;
-    
     calcTotal();
-
+    calcEnvio();
 }
-
 
 
 function showProductsList(array) {
@@ -24,35 +24,43 @@ function showProductsList(array) {
     let contenido = '';
     for (let i = 0; i < array.length; i++) {
         let category = array[i];
+        let conversion;
+        let moneda = category.currency;
+        let precio = category.unitCost;
+        if (moneda == "USD") {
+            conversion = precio * 40;
+        }
+        else {
+            conversion = category.unitCost;
+        }
 
-        let sub = category.unitCost * category.count;
+        let sub = conversion * category.count;
 
         contenido += `
         <tr>
             <td>${category.name}</td>
             <td><img src ='${category.src}' width="150px"></td>
             <td id="precio${i}">${category.currency} ${category.unitCost}</td>
-            <td><input style="width:60px;" onchange="calcSubtotal(${category.unitCost},${i})"
+            <td><input style="width:60px;" onchange="calcSubtotal(${conversion},${i})"
                 type="number" id="cantidad${i}" value="${category.count}" min="1"></td>
             <td><span class="subtotal" id="subtotal${i}" style="font-weight:bold;">${sub}</span></td>
         </tr>`
 
     }
-
     document.getElementById("listado").innerHTML = contenido;
 }
 function calcEnvio() {
     let total = document.getElementById("totalPesos").innerHTML;
     let envio;
-
     let array = document.getElementsByName("envio");
+    let pc;
     for (var i = 0; i < array.length; i++) {
         if (array[i].checked) {
-            envio = array[i].value;
+            pc = array[i].value;
+            envio = (total * pc) / 100;
         }
     }
-
-    let totalConEnvio = total + envio;
+    let totalConEnvio = Math.abs(-total - envio);
     let contenido = `
         <tr>
         <td>${total}</td>
@@ -63,8 +71,35 @@ function calcEnvio() {
     document.getElementById("totalEnvio").innerHTML = contenido;
 }
 
+let metodosDePago = document.getElementsByName("metodosDePago");
+for (let i = 0; i < metodosDePago.length; i++) {
+    metodosDePago[i].addEventListener("change", function () {
+        seleccionarPago();
+    });
+}
+
+function seleccionarPago() {
+    let metodosDePago = document.getElementsByName("metodosDePago");
+    for (let i = 0; i < metodosDePago.length; i++) {
+        if ((metodosDePago[i].value == "transferencia") && (metodosDePago[i].checked)) {
+            document.getElementById("transferencia").classList.remove("d-none");
+            document.getElementById("debito").classList.add("d-none");
+            document.getElementById("credito").classList.add("d-none");
+        }
+        else if ((metodosDePago[i].value == "debito") && (metodosDePago[i].checked)) {
+            document.getElementById("debito").classList.remove("d-none");
+            document.getElementById("transferencia").classList.add("d-none");
+            document.getElementById("credito").classList.add("d-none");
+        }
+        else if ((metodosDePago[i].value == "credito") && (metodosDePago[i].checked)) {
+            document.getElementById("credito").classList.remove("d-none");
+            document.getElementById("transferencia").classList.add("d-none");
+            document.getElementById("debito").classList.add("d-none");
+        }
+    }
+}
 document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(CART_INFO_URL).then(function (resultObj) {
+    getJSONData(CART_INFO_DESAFIO).then(function (resultObj) {
         if (resultObj.status === "ok") {
             ProductsArray = resultObj.data.articles;
 
@@ -72,11 +107,24 @@ document.addEventListener("DOMContentLoaded", function (e) {
             calcEnvio();
         }
     });
+
+  
+    let datosEnvio = document.getElementById("datosEnvio");
+    let modalPago = document.getElementById("modalPago");
+    datosEnvio.addEventListener('submit', function (e) {
+        if ((datosEnvio.checkValidity() === false)||(modalValidation() === false)) {
+            e.preventDefault();
+            e.stopPropagation();
+        }
+        datosEnvio.classList.add('was-validated');
+        modalPago.classList.add('was-validated');
+    });
+
     let array = document.getElementsByName("envio");
     for (var i = 0; i < array.length; i++) {
         array[i].addEventListener("change", function () {
             calcEnvio();
         });
     }
-
 });
+ 
